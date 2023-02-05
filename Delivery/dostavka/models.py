67a2785335d162d6ee16908f893ce71c62092b23
Constantin Shkutkov <shkutkoff@gmail.com>
2023-02-05 19:39:13 +0300

    start work with models

--- Delivery/dostavka/models.py
+++ Delivery/dostavka/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
+class Courier(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cafe = models.ForeignKey('Cafe', on_delete=models.CASCADE)
+
+
+class Cafe(models.Model):
+    address = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    ingredients = models.CharField(max_length=400)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Courier.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.courier.save()
